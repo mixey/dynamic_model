55bba92e2069bb9851e72d970355fe33acf7d708
mixey <mixey.jan@gmail.com>
2013-04-23 11:29:55 +0700

    * Code refactored

--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,8 @@
 import json
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from django.db.models import get_app, get_models
+from django.db.models import get_app, get_models, get_model
 from django.db import models
-from app1 import helper
 
 
 def main(request):
@@ -11,7 +10,7 @@ def main(request):
     try:
         app = get_app('app1')
         for model in get_models(app):
-            tables.append(model._meta.verbose_name_plural)
+            tables.append({"name": model._meta.module_name, "title": model._meta.verbose_name_plural})
 
     except:
         pass
@@ -20,16 +19,8 @@ def main(request):
                               {"tables": tables})
 
 def get_details(request):
-    table = request.POST.get("p_name", "")
-    app = get_app('app1')
-    store = []
+    model = get_model("app1", request.POST.get("p_name", ""))
     cols = []
-    model = None
-    for m in get_models(app):
-        if table == m._meta.verbose_name_plural:
-            model = m
-            break
-
     for field in model._meta.fields:
         t = "char"
         if (type(field) == models.IntegerField):
@@ -39,20 +30,16 @@ def get_details(request):
 
         cols.append({"name": field.name, "title": field.verbose_name, "type": t})
 
-    return HttpResponse(json.dumps({"store": helper.toJson(model.objects.values()), "cols": cols}), mimetype="application/json")
+    return HttpResponse(json.dumps({"store": json.loads(json.dumps(list(model.objects.values()), default=lambda item: str(item))), "cols": cols}), mimetype="application/json")
 
 def edit_row(request):
-    table = request.POST.get("p_table_name", "")
-    app = get_app('app1')
-    for model in get_models(app):
-        if table != model._meta.verbose_name_plural: continue
-
-        row = json.loads(request.POST.get("p_edit_row", ""))
-        if (row.has_key("id")):
-            item = model.objects.get(id=row["id"])
-        else:
-            item = model.objects.create()
-        item.__dict__.update(row)
-        item.save()
+    model = get_model("app1", request.POST.get("p_table_name", ""))
+    row = json.loads(request.POST.get("p_edit_row", ""))
+    if (row.has_key("id")):
+        item = model.objects.get(id=row["id"])
+    else:
+        item = model.objects.create()
+    item.__dict__.update(row)
+    item.save()
 
     return HttpResponse(json.dumps({"status": "OK"}), mimetype="application/json")
